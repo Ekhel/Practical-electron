890cbc07851af51c98c28b1aa19648878c391c25
ekhel <blackmanskill@gmail.com>
2020-11-16 03:23:21 +0900

    initialCommit

---
+++ main.js
@@ -0,0 +1,58 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindoe.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    const mainMenu = Menu.buildFromTemplate(MenuApp);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const MenuApp = [
+    {
+        label:'Data',
+        submenu: [
+            {
+                label:'Kepala Keluarga'
+            },
+            {
+                label:'Member Keluarga'
+            }
+        ]
+    },
+    {
+        label:'Settings',
+        submenu: [
+            {
+                label:'API',
+                submenu: [
+                    {
+                        label:'Kepala Keluarga'
+                    },
+                    {
+                        label:'Member Keluarga'
+                    }
+                ]
+            },
+            {
+                label:'Keluar',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
